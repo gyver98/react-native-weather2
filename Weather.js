c14a1ae31d3f7f826808982e7d39b16d73c044cc
Matthew Choi <matthewchoi7@gmail.com>
2017-11-03 15:58:20 +1100

    displaying weather data

--- Weather.js
+++ Weather.js
@@ -50,7 +50,7 @@ function Weather({ temp, weatherName }) {
       style={styles.container}>
       <View style={styles.upper}>
         <Ionicons color="white" size={144} name={weatherCases[weatherName].icon} />
-        <Text style={styles.temp}>{temp}</Text>
+        <Text style={styles.temp}>{temp}°</Text>
       </View>
       <View style={styles.lower}>
         <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
