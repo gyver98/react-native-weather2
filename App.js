ea91af8cd35f062f21bea977e11554ccc313fada
Matthew Choi <matthewchoi7@gmail.com>
2017-11-03 14:41:32 +1100

    getting weather data api

--- App.js
+++ App.js
@@ -2,13 +2,40 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from './Weather';
 
+const API_KEY = "908e1e83b536c6baafacb6c77fbb6d82";
+
 export default class App extends Component {
   state = {
-    isLoaded: true
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
   };
-  
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: error
+        })
+      }
+    );
+  }
+  getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        })
+      })
+  }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
@@ -17,6 +44,7 @@ export default class App extends Component {
         ): (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the weather data</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null }
           </View>
         )}
       </View>
@@ -37,9 +65,13 @@ const styles = StyleSheet.create({
     paddingRight: 10
 
   },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
+  },
   loadingText: {
     fontSize: 38,
-    marginBottom: 100,
-    
+    marginBottom: 24
   }
 });
